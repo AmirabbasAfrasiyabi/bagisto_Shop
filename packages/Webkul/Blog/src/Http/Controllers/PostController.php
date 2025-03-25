3c2799283000e60938c795484b704c922153c907
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2025-03-25 15:59:17 +0330

    Package Development(Controllers)

--- packages/Webkul/Blog/src/Http/Controllers/PostController.php
+++ packages/Webkul/Blog/src/Http/Controllers/PostController.php
@@ -1,12 +1,17 @@
 <?php
 
 namespace Webkul\Blog\Http\Controllers;
-
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Webkul\Blog\Http\Controllers\Admin\BlogController;
 use Webkul\Blog\Repositories\PostRepository;
 
-class PostController extends BlogController
+class PostController extends BaseController
 {
+
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     /**
      * PostRepository object
      *
